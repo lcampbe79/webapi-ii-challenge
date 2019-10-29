a4555c185e04ae8c17d7c23d5d415dfcff02be3e
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-29 12:57:45 -0700

    GET post by 'id'.

--- data/db-router.js
+++ data/db-router.js
@@ -12,6 +12,26 @@ router.get('/', (req, res) => {
     console.log(error);
     res.status(500).json({error: "The posts information could not be retrieved."})
   })
+});
+
+//GETs posts by id
+router.get('/:id', (req, res) => {
+  db.findById(req.params.id)
+  .then(post => {
+    if (post.length > 0) {
+      res.status(200).json(post)
+    } else {
+      res.status(400).json({message: "The post with the specified ID does not exist."})
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({error: "The posts information could not be retrieved."})
+  })
+})
+
+router.post('/', (req, res) => {
+
 })
 
 module.exports = router;
